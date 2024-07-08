cf907a3ff060b0f4622da3e056eeadeee78d5079
NathanOlgrin <matrica_1997@mail.ru>
2024-07-08 13:55:04 +0300

    Готовая задача 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,6 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Integer> list1 = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
-        System.out.println("Среднее значение чётных чисел списка " + list1.toString() + " равно " + list1.stream().filter(i -> i%2==0).mapToDouble(i -> i).average().orElse(Double.NaN));
+        System.out.println("Среднее значение чётных чисел списка " + list1 + " равно " + list1.stream().filter(i -> i%2==0).mapToDouble(i -> i).average().orElse(Double.NaN));
     }
 }
